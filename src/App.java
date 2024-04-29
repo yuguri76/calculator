848398be909d5f5fd32a44a133bc53bb3866db0e
gruri <ysy9976@naver.com>
2024-04-30 02:07:08 +0900

    level 1-3

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,5 +12,26 @@ public class App {
 
         System.out.print("사칙연산 기호를 입력하세요: ");
         char op = sc.next().charAt(0);
+
+        int result = 0;
+        switch(op) {
+            case '+' :
+                result = num1 + num2;
+                break;
+            case '-' :
+                result = num1 - num2;
+                break;
+            case '*' :
+                result = num1 * num2;
+                break;
+            case '/' :
+                if(num2 == 0){
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    break;
+                }
+                result = num1 / num2;
+                break;
+        }
+        System.out.println("결과: " + result);
     }
 }
