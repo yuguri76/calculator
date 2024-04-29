42d3cbfffcb13936b30ceb196838830128abb85f
gruri <ysy9976@naver.com>
2024-04-30 01:53:40 +0900

    level 1-1

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
+    }
+}
