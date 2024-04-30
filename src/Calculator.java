c32e7c3495aa893c3221eda0446530df897805b7
gruri <ysy9976@naver.com>
2024-04-30 16:04:01 +0900

    level 2-6

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 public class Calculator {
-    private List<Integer> results = new ArrayList<>();
+    private List<Integer> results;
+
+    public Calculator() {//생성자로 결과 저장할 리스트 초기화
+        this.results = new ArrayList<>();
+    }
 
     public int calculate(int num1, int num2, char op) throws Exception {
         int result = 0;
