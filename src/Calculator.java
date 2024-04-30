6eaed3958f8354677bb69c63f98e296756e98992
gruri <ysy9976@naver.com>
2024-04-30 10:56:34 +0900

    level 2-1

---
+++ src/Calculator.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class Calculator {
+    List<Integer> results = new ArrayList<>();
+
+    public int cal(int num1, int num2, char op) throws Exception {
+        int result = 0;
+
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    throw new Exception("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                }
+                result = num1 / num2;
+                break;
+        }
+        results.add(result);
+        return result;
+    }
+}
